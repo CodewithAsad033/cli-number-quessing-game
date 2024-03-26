1c50debf7f4d3cd587c938f368034934024a6c05
CodewithAsad033 <asadairf061@gmail.com>
2024-03-26 13:38:34 -0700

    Addig a Guessing number

---
+++ index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+
+console.log("\n\tWelcome to CodewithAsad - CLI Number Guessing Game\n\t");
+
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Enter your guess number (Number Limit from 1 to 5)",
+
+    },
+]);
+if(answer.userGuessedNumber === randomNumber ){
+console.log("congratulation ! You guess a correct number.");
+
+}
+else{
+    console.log("Sorry, you guess a wrong number");
+}
